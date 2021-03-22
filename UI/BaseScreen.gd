1ffcbc5e004338c65add16bc40217d0c9306bf59
quocdai-pc\dainq <quocdai.apps@gmail.com>
2021-03-22 07:54:48 +0700

    update

---
+++ UI/BaseScreen.gd
@@ -0,0 +1,10 @@
+extends CanvasLayer
+onready var tween = $Tween
+func appear():
+	tween.interpolate_property(self,"offset:x",500,0,0.5,Tween.TRANS_BACK,Tween.EASE_IN_OUT)
+	tween.start()
+func Disappear():
+	tween.interpolate_property(self,"offset:x",0,500,0.5,Tween.TRANS_BACK,Tween.EASE_IN_OUT)
+	tween.start()
+func _ready():
+	appear()
