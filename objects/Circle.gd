41ac946a1382c8dc75f86b1227a25de112a8bf70
Nguyễn Quốc Đại <dainq>
2021-03-24 16:33:43 +0700

    up

--- objects/Circle.gd
+++ objects/Circle.gd
@@ -2,29 +2,30 @@ extends Area2D
 
 onready var orbit_position = $Pivot/OrbitPosition
 onready var move_tween = $MoveTween
+
 enum MODES {STATIC, LIMITED}
 
 var radius = 100
 var rotation_speed = PI
 var mode = MODES.STATIC
 var move_range = 0
-var move_speed = 1.0
+var move_speed = 2.0
 var num_orbits = 3
 var current_orbits = 0
 var orbit_start = null
 var jumper = null
 
 func init(_position, level=1):
-	vảar _mode = settings.rand_weighted([10, level-1])
+	var _mode = settings.rand_weighted([10, level-1])
 	set_mode(_mode)
 	position = _position
-	var move_chance = clamp(level-10,0,9) / 10.0
+	var move_chance = clamp(level-10, 0, 9) / 10.0
 	if randf() < move_chance:
-		move_range = max (25,100 * rand_range(0.75,1.25) * move_change) * pow(-1,randi() % 2)
-		move_speed = max (2.5 - ceil(level/5)*0.25,0.75)
-	var small_change = min(0.9,max(0,(level-10) / 20.0))
-	if randf() < small_changce:
-		radius  = max(50,radius - level * rand_range(0.25,1.25))
+		move_range = max(25, 100 * rand_range(0.75, 1.25) * move_chance) * pow(-1, randi() % 2)
+		move_speed = max(2.5 - ceil(level/5) * 0.25, 0.75)
+	var small_chance = min(0.9, max(0, (level-10) / 20.0))
+	if randf() < small_chance:
+		radius = max(50, radius - level * rand_range(0.75, 1.25))
 	$Sprite.material = $Sprite.material.duplicate()
 	$SpriteEffect.material = $Sprite.material
 	$CollisionShape2D.shape = $CollisionShape2D.shape.duplicate()
@@ -47,7 +48,7 @@ func set_mode(_mode):
 			$Label.text = str(current_orbits)
 			$Label.show()
 			color = settings.theme["circle_limited"]
-	$Sprite.material.set_shader_param("color",color)
+	$Sprite.material.set_shader_param("color", color)
 			
 func _process(delta):
 	$Pivot.rotation += rotation_speed * delta
@@ -80,8 +81,10 @@ func capture(target):
 		
 func _draw():
 	if jumper:
-		var r = ((radius - 50) / num_orbits) * (1 + num_orbits - current_orbits)
-		draw_circle_arc_poly(Vector2.ZERO, r, orbit_start + PI/2,$Pivot.rotation + PI/2, settings.theme["circle_fill"])												
+		var r = ((radius - 10) / num_orbits) * (1 + num_orbits - current_orbits)
+		draw_circle_arc_poly(Vector2.ZERO, r, orbit_start + PI/2,
+							$Pivot.rotation + PI/2, settings.theme["circle_fill"])
+												
 func draw_circle_arc_poly(center, radius, angle_from, angle_to, color):
 	var nb_points = 32
 	var points_arc = PoolVector2Array()
@@ -92,11 +95,12 @@ func draw_circle_arc_poly(center, radius, angle_from, angle_to, color):
 		var angle_point = angle_from + i * (angle_to - angle_from) / nb_points - PI/2
 		points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * radius)
 	draw_polygon(points_arc, colors)
+
 func set_tween(object=null, key=null):
 	if move_range == 0:
 		return
 	move_range *= -1
-	move_tween.interpolate_property(self,"position:x",
-				position.x,position.x + move_range,
-				move_speed,Tween.TRANS_QUAD,Tween.EASE_IN_OUT)
+	move_tween.interpolate_property(self, "position:x",
+				position.x, position.x + move_range,
+				move_speed, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	move_tween.start()
