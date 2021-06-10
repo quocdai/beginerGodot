d7b5cd88f2f5d17cb275fefd674d5c2e69eafc47
Administrator <admin@example.com>
2021-06-11 02:10:22 +0700

    update

--- Circle.gd
+++ Circle.gd
@@ -8,13 +8,13 @@ var rotation_speed = PI
 
 func _ready():
 	init()
-
+	
 func init(_radius=radius):
 	radius = _radius
 	$CollisionShade2D.shape = $CollisionShade2D.shade.duplicate()
 	$CollisionShade2D.shape.radius = radius
 	var img_size = $sprite.texture.get_size().x / 2
-	$Sprite.scale = Vector2(1,1) * radius / img_size
+	$Sprite.scale = Vector2.ONE * radius / img_size
 	orbit_position.position.x = radius + 25
 func _process(delta):
 	$Pivot.rotation += rotation_speed * delta
