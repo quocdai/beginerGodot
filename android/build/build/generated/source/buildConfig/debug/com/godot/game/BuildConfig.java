b9e3584d415c2b5c575579c37059b3b2c4190523
mabu <mabu>
2021-06-28 16:28:16 +0700

    updateupdate

--- android/build/build/generated/source/buildConfig/debug/com/godot/game/BuildConfig.java
+++ android/build/build/generated/source/buildConfig/debug/com/godot/game/BuildConfig.java
@@ -5,7 +5,7 @@ package com.godot.game;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.quocdai.abcjump";
+  public static final String APPLICATION_ID = "com.mabu.abcjump";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
